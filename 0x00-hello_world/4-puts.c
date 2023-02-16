2793c0657c5503412b8ce0e795036fe54fe58c92
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-16 12:35:04 -0800

    betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main()
+int main(void)
 	/**
 	 * printout a statement  followed by a new line
 	 *return 0
 	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
