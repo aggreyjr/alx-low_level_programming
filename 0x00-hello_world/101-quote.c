e3d2bc884df383eea1c73fe5f5b12e4cce0481d4
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-16 15:07:09 -0800

    go

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
 #include<unistd.h>
-int main()
+/**
+ * main - A C program that prints a line to the stderr
+ * Return: 1(success)
+ */
+int main(void)
 {
 	fputs("and that piece of art is useful\" -Dora Korpar,2015-10-19\n",stdout);
-	return 1;
+	return (1);
 }
