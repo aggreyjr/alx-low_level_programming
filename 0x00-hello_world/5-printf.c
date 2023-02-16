e2d91e4112ad9e5d23424b7ee2f58a5cd36c23ce
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-16 14:19:40 -0800

    good

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that outputs a line using print function
  * Return: 0(success)
  */
 int main(void)
 {
-	printf("\"with proper grammer, but the outcome is a piece of art,\n");stderr
+	printf("\"with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
