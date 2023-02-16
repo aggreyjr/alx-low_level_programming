196806f9a7ce913f298d80d9ecde0ebb7a35c6f3
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-16 12:32:08 -0800

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
+	/**
+	 * printout a statement  followed by a new line
+	 *return 0
+	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
