0a075d9021c164ef0dfbe41e2c5ad53722b24a38
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-17 08:10:58 -0800

    good

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a Program for caps
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
