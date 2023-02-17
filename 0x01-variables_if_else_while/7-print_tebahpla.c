80dcd57852a34b1f0c1c5b1a73a136f76b12bca9
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-17 10:21:30 -0800

    go

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A c program
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	char low;
+	
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
