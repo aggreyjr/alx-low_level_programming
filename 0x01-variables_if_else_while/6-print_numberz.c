f2f8bb246f6446d6dadc810c3bd4ef9a40a534c1
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-17 10:15:17 -0800

    go

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Aprogram in c
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
