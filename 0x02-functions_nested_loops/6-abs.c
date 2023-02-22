f9c9a934076308b8ffa9b802473b247b5cdd9e56
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-22 05:34:13 -0800

    kool

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,12 +8,12 @@
 
 int _abs(int c)
 {
-	if (c < 0)
+	if (c >= 0)
 	{
-		int abs_val;
-
-			abs_val = c * -1;
-		return (abs_val);
+		return (c);
 	}
-	return (c);
+	else
+	{
+		return (c * -1);
+        }
 }
