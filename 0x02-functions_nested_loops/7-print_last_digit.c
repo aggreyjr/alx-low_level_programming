fb0f749377091c8402a1770a42664eac096ed99b
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-22 01:53:16 -0800

    go

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digit of the number
  * @n: the number to be treated
- * return: value of last digit
+ * Return: value of last digit
  */
 
 int print_last_digit(int n)
