a8af6bb8fe672b6a368be98bf62f360f89341819
aggreyjr <tukashabaaggrey89@gmail.com>
2023-02-23 13:07:40 -0800

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,11 +10,8 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming:(\n");
+
 	i = 0;
-	/*while (i < 10)*/
-	 /* {*/
-	 /* putchar(i);*/
-	 /* }*/
 	printf("Infinite loop avoided!\n");
 
 	return (0);
