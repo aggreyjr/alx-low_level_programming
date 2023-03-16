0bea535e2d1198324272b94b08b9f8bd24723f0c
aggreyjr <tukashabaaggrey89@gmail.com>
2023-03-16 14:57:38 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,11 +12,9 @@ int is_digit(char *s)
 	int i = 0;
 
 	while (s[i])
-	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
-	}
 	return (1);
 }
 /**
@@ -29,9 +27,7 @@ int _strlen(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
 /**
@@ -64,21 +60,17 @@ int main(int argc, char *argv[])
 
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
@@ -86,7 +78,6 @@ int main(int argc, char *argv[])
 	{
 		if (result[i])
 			a = 1;
-
 		if (a)
 			_putchar(result[i] + '0');
 	}
