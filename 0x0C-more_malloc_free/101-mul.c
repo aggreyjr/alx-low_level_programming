c13929dcb1f0d1e604c5ae2e9b55fc29ef20df50
aggreyjr <tukashabaaggrey89@gmail.com>
2023-03-16 16:40:48 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,137 +1,40 @@
+#include <stdlib.h>
 #include "main.h"
-
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
- * Return: void
+ * realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated with call to '''
+ * malloc : malloc(old_size)'''.
+ * @old_size: size in bytes allocated for ptr
+ * @new_size: size in bytes of new memory block
+ * Return: pointer to new mem block, NULL or ptr
  */
-void _print(char *str, int l)
+void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i, j;
+	char  *nptr;
+	unsigned int i;
 
-	i = j = 0;
-	while (i < l)
+	if (new_size == old_size)
+		return (ptr);
+	if ((new_size == 0) && (ptr != NULL))
 	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - l)
-			_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-	free(str);
-}
-/**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
- * Return: pointer to dest, or NULL on failure
- */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
+		free(ptr);
 		return (NULL);
 	}
-	return (dest);
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+	}
+	if (new_size > old_size && (ptr != NULL))
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (nptr);
+		for (i = 0; i < old_size; i++)
+			nptr[i] = *((char *)ptr + 1);
+		free(ptr);
+	}
+	return (nptr);
 }
-/**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
- * Return: 0 for digits, 1 for non-digits
- */
-int check_for_digits(char **av)
-{
-	int i, j;
 
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
-}
-/**
- * init - initializes a string
- * @str: string to initialize
- * @l: length of string
- * Return: void
- */
-void init(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '/0';
-}
-/**
- * main - multiply two numbers
- * @argc: number of arguments
- * @argv: argument vector
- * Return: 0, or exit status of 98 if failure
- */
-int main(int argc, char *argv[])
-{
-	int l1, l2, ln, ti, i;
-	char *a;
-	char *t;
-	char e[] = "Error\n";
-
-	if (argc != 3 || check_for_digits(argv))
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][12]; 12++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NUUL)
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti-- i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
-	}
-	_print(a, ln - 1);
-	return (0);
-}
