6675d9ca427f1cd8243b9352e54b190d6909cb39
aggreyjr <tukashabaaggrey89@gmail.com>
2023-04-17 04:44:47 -0700

    try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,15 +25,15 @@ char *create_buff(char *file)
 /**
  * close_file - closes files
  * @fd: file descriptor
- *
  * Return: nothing
  */
+
 void close_file(int fd)
 {
-	int p;
+	int r;
 
-	p = close(fd);
-	if (p == -1)
+	r = close(fd);
+	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -44,13 +44,13 @@ void close_file(int fd)
  * main - program that copies the content of a file to another file
  * @argc: number of arguments passed to main
  * @argv: string
- *
  * Return: 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	int fd_to, fd_from, read_size, write_size;
-	char *buff;
+	char *buf;
 
 	if (argc != 3)
 	{
@@ -58,34 +58,34 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buff = create_buff(argv[2]);
+	buf = create_buff(argv[2]);
 
 	fd_from = open(argv[1], O_RDONLY);
-	read_size = read(fd_from, buff, 1024);
+	read_size = read(fd_from, buf, 1024);
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (fd_from == -1 || read_size == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buff);
+			free(buf);
 			exit(98);
 		}
 
-		write_size = write(fd_to, buff, read_size);
+		write_size = write(fd_to, buf, read_size);
 		if (fd_to == -1 || write_size == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buff);
+			free(buf);
 			exit(99);
 		}
 
-		read_size = read(fd_from, buff, 1024);
+		read_size = read(fd_from, buf, 1024);
 		fd_to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (read_size > 0);
 
-	free(buff);
+	free(buf);
 	close_file(fd_from);
 	close_file(fd_to);
 	return (0);
